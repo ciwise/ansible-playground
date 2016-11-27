10df1d76d76b68b7d7db872ac5e271d364550d9e
David L. Whitehurst <david@ciwise.com>
2016-11-27 14:33:08 -0500

    added ciwise-4

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+resource "digitalocean_droplet" "ciwise-4" {
+    image = "debian-8-x64"
+    name = "ciwise-4"
+    region = "nyc2"
+    size = "512mb"
+    private_networking = true
+    ssh_keys = [
+      "${var.ssh_fingerprint}"
+    ]
+}
+
+resource "digitalocean_domain" "dns-sandbox" {
+  name = "sandbox.ciwise.com"
+  ip_address = "${digitalocean_droplet.ciwise-4.ipv4_address}"
+}
+
 resource "digitalocean_droplet" "ciwise-3" {
     image = "debian-8-x64"
     name = "ciwise-3"
@@ -8,7 +24,7 @@ resource "digitalocean_droplet" "ciwise-3" {
       "${var.ssh_fingerprint}"
     ]
 
-    provisioner "file" {
+    provisioner "file" "ansible-load" {
        connection {
           type = "ssh"
 	  user = "root"
@@ -17,7 +33,17 @@ resource "digitalocean_droplet" "ciwise-3" {
       source="script.sh"
       destination = "/tmp/script.sh"
     }
-    
+
+    provisioner "file" "ansible-playbook" {
+       connection {
+          type = "ssh"
+	  user = "root"
+	  private_key = "~/.ssh/id_rsa"
+       }
+      source="playbook.yml"
+      destination = "/root/playbook.yml"
+    }
+
     provisioner "remote-exec" {
       connection {
          type = "ssh"
@@ -25,18 +51,23 @@ resource "digitalocean_droplet" "ciwise-3" {
           private_key = "~/.ssh/id_rsa"
        }
        inline = [
-         "chmod 777 /tmp/script.sh", 
+         "chmod 777 /tmp/script.sh",
 	 "/tmp/script.sh"
        ]
     }
 
 }
 
-resource "digitalocean_domain" "default" {
+resource "digitalocean_domain" "dns-ansible" {
   name = "ansible.ciwise.com"
   ip_address = "${digitalocean_droplet.ciwise-3.ipv4_address}"
 }
 
-output "ip_ciwise" {
+
+output "ip_ciwise_3" {
   value = ["${digitalocean_droplet.ciwise-3.ipv4_address}"]
+}
+
+output "ip_ciwise_4" {
+  value = ["${digitalocean_droplet.ciwise-4.ipv4_address}"]
 }
--- script.sh
+++ script.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
+# written by David L. Whitehurst for CI Wise
+#
+# This script is sent over to a new Digital Ocean droplet called (DNS)
+# ansible.ciwise.com and used to set-up a second droplet called (DNS)
+# sandbox.ciwise.com with MySQL.
+#
 
+# Using APT install everything we need
 apt-get install -y software-properties-common
 apt-add-repository ppa:ansible/ansible -y
 apt-get update -y
 apt-get install ansible -y
+
+cd /root
+echo '[sandbox]' >> /etc/ansible/hosts
+echo 'sandbox.ciwise.com' >> /etc/ansible/hosts
+
+# next step to go to ansible.ciwise.com and ssh-keygen and copy
+# id_rsa.pub to authorized_keys on sandbox.ciwise.com. Then you
+# can run Ansible playbook that's already there.
